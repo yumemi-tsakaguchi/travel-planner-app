0ea417d792110a137cb0891c57e4a1041c6ac7c3
Tomonori Sakaguchi <t_sakaguchi@yumemi.co.jp>
2025-08-05 08:27:06 +0900

    Headerコンポーネントにホームページ判定を追加し、ナビゲーションリンクの表示を条件付きに変更

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import styles from './Header.module.css'
 
 const Header = () => {
+  const location = useLocation()
+  const isHomePage = location.pathname === '/'
+
   return (
     <header className={styles.header}>
       <div className={styles.container}>
@@ -9,9 +12,11 @@ const Header = () => {
           <h1>旅プランAI</h1>
         </Link>
         <nav className={styles.nav}>
-          <Link to="/" className={styles.navLink}>
-            ホーム
-          </Link>
+          {!isHomePage && (
+            <Link to="/" className={styles.navLink}>
+              ホーム
+            </Link>
+          )}
           <Link to="/plan" className={styles.navLink}>
             プラン作成
           </Link>
